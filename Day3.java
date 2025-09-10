b60b18d395b6f70ae65eedb8312934b474d8308e
aldiahayy <aldisalam0756@gmail.com>
2025-09-10 21:59:26 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,13 @@
+public class day3 {
+    public static void main(String[] args) {
+        System.out.println("Halo bang");//memberi komentar hanya satu baris
+        System.out.print("Hari ke 3 dari 100 hari");
+        System.out.println("");
+        System.out.printf("|%-8s Mantap|"," Kata-kata : ");
+        /*memberi komentar
+        lebih dari 
+        satu baris*/
+        
+    }
+    
+}
