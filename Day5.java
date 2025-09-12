26600c0c5f9ee53d23077a6d1004336918b3263a
aldiahayy <aldisalam0756@gmail.com>
2025-09-12 20:08:55 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,14 @@
+public class day5 {
+ public static void main(String[] args) {
+    byte umur = 19;
+    short tahunLahir = 2006;
+    int gaji = 2300000;
+    long warisan = 3000000000L;
+    
+     System.out.println("byte = " + umur);
+     System.out.println("short = " + tahunLahir);
+     System.out.println("integer = " + gaji);
+     System.out.println("long = " + warisan);
+}
+   
+}
