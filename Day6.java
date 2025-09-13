5e1d2bf1fe70bb02b1d0dde961727e5d9bf6737f
aldiahayy <aldisalam0756@gmail.com>
2025-09-13 18:50:45 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,10 @@
+public class day6 {
+   public static void main(String[] args) {
+    float xFloat = 3.142857f;
+    double xDouble = 256.14600;
+    
+       System.out.println("Nilai float = " + xFloat);
+       System.out.println("Nilai double = " + xDouble);
+}
+ 
+}
