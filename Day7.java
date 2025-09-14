21d2e209bc39d3991c776d4cfe53afc3181c7712
aldiahayy <aldisalam0756@gmail.com>
2025-09-14 20:43:47 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,20 @@
+public class day7 {
+    public static void main(String[] args) {
+        char A = 'あ';
+        char ri = 'り';
+        char ga = 'が';
+        char t = 'と';
+        char u = 'う';
+
+        boolean hari = true;
+
+        System.out.print(A);
+        System.out.print(ri);
+        System.out.print(ga);
+        System.out.print(t);
+        System.out.print(u);
+        System.out.println();
+
+        System.out.println("Apakah besok hari senin? " + hari);
+    }
+}
