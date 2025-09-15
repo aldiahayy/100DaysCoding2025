0d80491b23360668a025a6b7f7463584c3c6e22b
aldiahayy <aldisalam0756@gmail.com>
2025-09-15 21:34:19 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,12 @@
+public class day8 {
+    public static void main(String[] args) {
+        String masukISC = "Karena ingin mempelajari hal-hal baru yang berkaitan dengan jurusan informatika";
+        String pilihInformatika = "Karena informatika pilihan orang tua dan kakak saya";
+        String pilihJurusan = "Tidak sama sekali";
+        String mataKuliah = "Karena saya belum mendapatkan alasan mengapa saya harus belajar semua ini";
+        System.out.println("Alasan masuk ISC? " + masukISC);
+        System.out.println("Alasan memilih prodi Informatika? " + pilihInformatika);
+        System.out.println("Apakah merasa salah pilih jurusan atau tidak? " + pilihJurusan);
+        System.out.println("Kendala mata kuliah? " + mataKuliah);
+    }
+}
