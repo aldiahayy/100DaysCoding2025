fb141b4eb4c5d083d2a8d1a6b9475265340f36b1
aldiahayy <aldisalam0756@gmail.com>
2025-09-17 21:15:00 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,7 @@
+public class day10 {
+    public static void main(String[] args) {
+    final int jam = 24; // karana 1 hari itu 24 jam 
+    int setengah_hari = 24/2;
+        System.out.println("setengah hari itu sama dengan " + setengah_hari + " jam");
+}
+}
