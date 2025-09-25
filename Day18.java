d4055583f5f8a5585227764d80755aa16b2d6916
aldiahayy <aldisalam0756@gmail.com>
2025-09-25 16:21:37 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,8 @@
+public class day18 {
+public static void main(String[] args) {
+    int hargaBarang = 150000;
+    double pajak = 16500.50;
+    double totalBayar = hargaBarang + pajak;
+    System.out.println("Total bayar adalah " + totalBayar);
+}    
+}
