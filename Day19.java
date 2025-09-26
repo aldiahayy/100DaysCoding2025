f510f8fc828f63a9d424db2141de9589185b9e12
aldiahayy <aldisalam0756@gmail.com>
2025-09-26 20:25:58 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,20 @@
+public class day19 {
+public static void main(String[] args) {
+    int angkaBesarLong = 123456789;
+    float angkaBesarFloat = 3.1428571428571f;
+    
+    short angkaKecilShort = (short)angkaBesarLong;
+    int angkaKecilInt = (int)angkaBesarFloat;
+    
+    System.out.println("Konversi dari integer ke short : " + angkaKecilShort);
+    System.out.println("Konversi dari float ke int : " + angkaKecilInt);
+    
+    long shortKeLong = angkaKecilShort;
+    double intKeDouble = angkaKecilInt;
+    
+    System.out.println("Konversi dari short ke long : " + shortKeLong);
+    System.out.println("Konversi dari int ke Double : " + intKeDouble);
+    
+}
+    
+}
