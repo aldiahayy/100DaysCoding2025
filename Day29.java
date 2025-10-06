e6f4309c3347ba2e949ad30025de0057e6223c26
aldiahayy <aldisalam0756@gmail.com>
2025-10-06 23:20:06 +0800

    Day29.java

--- Day29.java
+++ Day29.java
@@ -3,17 +3,19 @@ public class day29 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println(">>> SELAMAT DATANG DI WAHANA KEMATIAN <<<");
-        System.out.println("  === Silahkan Masukkan Persyaratan ===");
+        System.out.println(">>> Silahkan Masukkan Persyaratan <<<");
         System.out.print("Masukkan tinggi badan : ");
         int tb = sc.nextInt();
         System.out.print("Masukkan berat badan : ");
         int bb = sc.nextInt();
         
+        System.out.println(">>> Data Pengunjung <<<");
         System.out.printf("%-15s : %d cm %n", "Tinggi Badan", tb);
         System.out.printf("%-15s : %d kg %n", "Berat Badan", bb);
-              
-        boolean deteksiTb = tb > 150;
-        boolean deteksiBb = bb < 80;
-        System.out.println("Apakah kamu diizinkan naik? " + (deteksiTb && deteksiBb));
+    
+        System.out.println("Apakah tinggi badan nya memenuhi? " + (tb > 150));
+        System.out.println("Apakah tinggi badan nya memenuhi? " + (bb < 80));
+        System.out.println("Apakah kamu diizinkan naik? " + ((tb > 150) & (bb < 80)));
+       
         }
     }
