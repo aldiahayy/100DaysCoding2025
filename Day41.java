1ee5d469c750376b60439b4c332b878f869ee056
aldiahayy <aldisalam0756@gmail.com>
2025-10-19 13:26:54 +0800

    Day41.java

--- Day41.java
+++ Day41.java
@@ -1,5 +1,51 @@
-public class day41 {
-public static void main(String[] args) {
-    System.out.println("kosong");
+package evaluasiDua;
+import java.util.Scanner;
+public class soal1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--- Program Hitung Diskon ---");
+        System.out.print("Masukkan jumlah barang : ");
+        int barang = sc.nextInt();
+        System.out.print("Masukkan harga per barang : ");
+        double harga = sc.nextDouble();
+        System.out.print("Apakah Anda member? (ketik 'true' atau 'false') : ");
+        String status = sc.next();
+
+        double beli = barang * harga;
+        boolean status2 = Boolean.parseBoolean(status);
+        double totalAkhir = beli;
+        boolean deteksi = beli >= 250000 && status2;
+        System.out.println("\n--- Ringkasan Pembelian ---");
+        System.out.printf("%-17s : Rp %.2f %n", "Total Pembelian", beli);
+  
+        if (deteksi) {
+            double diskon = beli * 0.10;
+            totalAkhir -= diskon;
+            System.out.printf("%-17s : %b %n", "Berhak Diskon 10%", deteksi);
+            System.out.printf("%-17s : Rp %.2f %n", "Jumlah Diskon", diskon);
+        } else {
+            System.out.printf("%-17s : %b %n", "Berhak Diskon 10%", deteksi);
+        }
+            System.out.printf("%-17s : Rp %.2f %n", "Total Bayar", totalAkhir);
+    }
 }
+
+package evaluasiDua;
+import java.util.Scanner;
+public class soal2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan jam mulai pengerjaan : ");
+        int s = sc.nextInt();
+        System.out.print("Masukkan lama pengerjaan tugas : ");
+        int l = sc.nextInt();
+        int waktu = s + l;
+        int h = waktu % 24;
+        if (h < 9) {
+            System.out.println((9 - h) + " jam");
+        } else {
+            System.out.println("0 jam");
+        }
+    }
 }
+
