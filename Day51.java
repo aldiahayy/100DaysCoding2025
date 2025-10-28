a42956294c4d79421e95fbae446a36285f18dae3
aldiahayy <aldisalam0756@gmail.com>
2025-10-28 23:06:03 +0800

    Day51.java

---
+++ Day51.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class day51 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Masukkan batas perkalian: ");
+    int batas = sc.nextInt();
+    System.out.print("Masukkan angka yang ingin dikalikan: ");
+    int angka = sc.nextInt();
+    
+    for (int i = 1; i <= batas; i++) {
+        System.out.println(i + " x " + angka + " = " +(angka * i));       
+    }
+}   
+}
