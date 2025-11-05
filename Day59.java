700953509ce9fbddb5f16b26771e11967f94b68e
aldiahayy <aldisalam0756@gmail.com>
2025-11-05 19:12:01 +0800

    Day59.java

--- Day59.java
+++ Day59.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 public class day59 {
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.print("Masukan batas angka: ");
-    int n = sc.nextInt();
-    System.out.println("ganjil");
-    for (int i = 1; i <= n; i+=2) {
-        System.out.print(i+" ");            
+    System.out.print("Masukan batas angka(N): ");
+    int N = sc.nextInt();
+    System.out.print("Angka ganjil: ");
+    for (int i = 1; i <= N; i++) {
+        if (i%2==1) {
+         System.out.print(i+" ");    
+        }           
         }
-    System.out.println("\ngenap");
-    for (int j = 1; j <= n; j++) {
+    System.out.print("\nAngka genap: ");
+    for (int j = 1; j <= N; j++) {
         if (j%2==0) {
             System.out.print(j+" ");
         }
-    }
+        }
     }
 }
