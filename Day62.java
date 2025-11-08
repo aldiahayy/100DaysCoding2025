cf0e1f482f685371d4a6c23a0a70646fd811aa94
aldiahayy <aldisalam0756@gmail.com>
2025-11-08 20:36:01 +0800

    Day62.java
    Display the numbers being summed and format the output.

--- Day62.java
+++ Day62.java
@@ -5,10 +5,14 @@ public static void main(String[] args) {
     System.out.print("Masukan batas angka(N): ");
     int N = sc.nextInt();
     int total = 0;
-    System.out.print("Total penjumlahan dari 1 sampai " +N+" adalah ");
     for (int i = 1; i <= N; i++) {
+        System.out.print(i);
+        if (i< N) {
+             System.out.print(" + ");
+        }
         total+=i;
     }
-    System.out.println(total);
+     System.out.println(" = "+total);
+     System.out.println("Total penjumlahan dari 1 sampai " +N+" adalah " + total);
 }   
 }
