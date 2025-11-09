72c4d264b698035eb82353d4e5385743ab150222
aldiahayy <aldisalam0756@gmail.com>
2025-11-09 18:44:39 +0800

    Day63.java

---
+++ Day63.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class day63 {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Masukan batas angka(N): ");
+    int N = sc.nextInt();
+    int total = 1;
+    for (int i = 1; i <= N; i++) {
+        total*=i;
+        System.out.print(i);
+        if (i<N) {
+            System.out.print(" x ");
+        }
+    }
+    System.out.println(" = " + total);
+    System.out.println("Jadi total perkalian dari 1 sampai " + N + " adalah " + total);
+}
+}
