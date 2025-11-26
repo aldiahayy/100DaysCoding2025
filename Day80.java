a24f93de6ad9ea9c6cabb0e219c2f6d5c0af2ac1
aldiahayy <aldisalam0756@gmail.com>
2025-11-26 23:19:01 +0800

    Day80.java

---
+++ Day80.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class day80 {
+public static void main(String[] args) {
+    String buah[] = {"apel", "anggur", "stroberi", "pisang", "mangga"};
+    System.out.println("Buah terenak  : " + buah[1]);
+    System.out.println("Buah terakhir : " + buah[4]);
+    System.out.println("Saya bosan dengan buah " + buah[3] + ". Ganti dengan durian");
+    buah[3] = "durian";
+    System.out.println("Buah yang ke-4 sekarang adalah " +  buah[3]);
+    System.out.println("Banyak buah adalah " + buah.length);   
+}
+}
