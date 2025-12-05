6a67757feca258e0ebeed6c42aab3ffb978a4fa3
aldiahayy <aldisalam0756@gmail.com>
2025-12-05 20:41:26 +0800

    Day89.java

---
+++ Day89.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class day89 {
+public static void main(String[] args) {
+    sapaDefault();
+}
+public static void sapaDefault(){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Masukan pesan: ");
+    String pesan = sc.nextLine();
+    System.out.println(pesan);
+} 
+}
