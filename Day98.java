b8b0482243ce4b0791fbf1caa85a782d6a0b211c
aldiahayy <aldisalam0756@gmail.com>
2025-12-14 23:15:15 +0800

    Day98.java
    Added user input for Fibonacci sequence length and output message.

--- Day98.java
+++ Day98.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 public class day98 {
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    System.out.print("Masukan jumlah deret: ");
     int n = sc.nextInt();
     int a = 0;
     int b = 1;
+    System.out.print("Deret fibonanci : ");
     for (int i = 0; i < n; i++) {
         System.out.print(a+" ");
     int c = a+b;
